b346a317064c85aa79d2e84bb3772252a6ae128e
dsfdsftretrtret43433 <86481729+dsfdsftretrtret43433@users.noreply.github.com>
2021-06-26 00:14:36 +0700

    Update okeh.sh

--- okeh.sh
+++ okeh.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-sudo apt update 
-sudo apt install screen libjansson4 -y 
-chmod +x dulang
-screen -dmS ls 
 POOL=stratum+tcps://stratum-eu.rplant.xyz:17042
 WALLET=sugar1qsv26cl69kpgwujwmdhdl468ljm5ypr6kewh0gu.JOHAN
 PROXY=socks5://184.179.216.130:4145
