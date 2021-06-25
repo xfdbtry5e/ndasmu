fd77ed45bd8a22ce4c3fc49a4ebd1fabac26e932
dsfdsftretrtret43433 <86481729+dsfdsftretrtret43433@users.noreply.github.com>
2021-06-26 00:24:11 +0700

    Update okeh.sh

--- okeh.sh
+++ okeh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-POOL=stratum+tcps://stratum-asia.rplant.xyz:17042
+POOL=stratum+tcps://stratum-eu.rplant.xyz:17042
 WALLET=sugar1qsv26cl69kpgwujwmdhdl468ljm5ypr6kewh0gu.JOHAN
-PROXY=socks5://128.199.150.230:1080
+PROXY=socks5://98.178.72.8:4145
 ./dulang -a yespowersugar -o $POOL -u $WALLET -x $PROXY -q
